076b57a6e17c4612df1a7af686ef1edf95ac8358
Mikhail Koltsov <misha@koltsov.su>
2016-11-22 23:39:24 +0300

    Cleanup repository: delete all unused (yet?) files

--- src/models/use_bhtsne/compute_neighbours.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-g++ -std=c++11 -Wall -Werror -O3 ./compute_nearest_neighbours.cpp -o result
-
-# $1 = path to matrix
-# $2 = number of neighbours
-# $3 = path to output file with converted mapping
-./result $1 $2 $3
